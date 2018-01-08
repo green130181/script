aa5b8adc32638946959f3ed51ce1689a180c7c60
green130181 <mail.zhangzhao@gmail.com>
2018-01-08 20:39:34 +0800

    add app log script
    Signed-off-by: zhangzhao5 <zhangzhao5@xiaomi.com>

---
+++ bash-shell/fl.sh
@@ -0,0 +1,8 @@
+fastboot flash boot_ab boot.img
+fastboot flash dtbo_ab dtbo.img
+fastboot flash system_ab system.img
+fastboot flash userdata userdata.img
+fastboot flash vendor_ab vendor.img
+fastboot flash vbmeta_ab vbmeta.img
+fastboot flash cust cust.img
+fastboot reboot
